81897ed71d494c9a76fe0eb7be997731c7a980ee
shivam7374 <shivamsingla491@gmail.com>
2020-09-17 15:01:39 +0530

    FacebookLogin

---
+++ FacebookLogin.py
@@ -0,0 +1,13 @@
+# import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+user_name = "YOUR EMAILID"
+password = "YOUR PASSWORD"
+driver = webdriver. Chrome()
+driver.get("https://www.facebook.com")
+element = driver.find_element_by_id("email")
+element.send_keys(user_name)
+element = driver.find_element_by_id("pass")
+element.send_keys(password)
+element.send_keys(Keys.RETURN)
+element.close()
